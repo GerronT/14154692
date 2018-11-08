470577ab603e78133ac483bf2f455dbc3bee8763
Gerron Tinoy <gerrontinoy@gerrons-macbook-pro.local>
2018-11-08 11:41:03 +0000

    Initial Commit

---
+++ 14154692_MBCourseworkTests/_4154692_MBCourseworkTests.swift
@@ -0,0 +1,36 @@
+//
+//  _4154692_MBCourseworkTests.swift
+//  14154692_MBCourseworkTests
+//
+//  Created by Gerron Tinoy on 08/11/2018.
+//  Copyright © 2018 Gerron Tinoy. All rights reserved.
+//
+
+import XCTest
+@testable import _4154692_MBCoursework
+
+class _4154692_MBCourseworkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
