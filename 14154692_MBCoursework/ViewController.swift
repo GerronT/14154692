4b1f5b4ef68666f2dccac21fefa58d16fc468ba1
Gerron Tinoy <gerrontinoy@gerrons-macbook-pro.local>
2018-11-16 14:07:59 +0000

    Added avatar alternating animation

--- 14154692_MBCoursework/ViewController.swift
+++ 14154692_MBCoursework/ViewController.swift
@@ -10,12 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var boatImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        // sample change
-        // change again
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Animation of walking
+        var imageArray: [UIImage]!
+        
+        imageArray = [UIImage(named: "boat_1.png")!,
+                      UIImage(named: "boat_2.png")!,
+                      UIImage(named: "boat_3.png")!,
+                      UIImage(named: "boat_4.png")!,
+                      UIImage(named: "boat_5.png")!,
+                      UIImage(named: "boat_6.png")!,
+                      UIImage(named: "boat_7.png")!,
+                      UIImage(named: "boat_8.png")!,
+                      UIImage(named: "boat_9.png")!]
+        
+        boatImage.image = UIImage.animatedImage(with: imageArray, duration: 1)
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
