c4c99e688f8e5ab0d1fac048e015aae80617c2ee
Gerron Tinoy <gerrontinoy@gerrons-mbp.herts.ac.uk>
2018-12-06 18:44:14 +0000

    added coins

--- 14154692_MBCoursework/ViewController.swift
+++ 14154692_MBCoursework/ViewController.swift
@@ -10,32 +10,25 @@ import UIKit
 
 protocol subviewDelegate {
     func boatDragged()
-    
-    
-    
 }
-class ViewController: UIViewController, subviewDelegate {
+class ViewController: UIViewController, subviewDelegate, UICollisionBehaviorDelegate {
     var dynamicAnimator: UIDynamicAnimator!
     var dynamicItemBehavior: UIDynamicItemBehavior!
     var collisionBehavior: UICollisionBehavior!
     
     var scoreValue: Int!
     var scoreTimer: Timer!
-    var collisionTimer: Timer!
     var gameTimer: Timer!
     
     @IBOutlet weak var boatImage: DraggedImageView!
-    @IBOutlet weak var rock: UIImageView!
-    @IBOutlet weak var rock2: UIImageView!
-    @IBOutlet weak var rock3: UIImageView!
     @IBOutlet weak var score: UILabel!
     
     var backgroundImage: UIImageView!
     
     var obstacleArray: [UIImageView]!
+    var coinsArray: [UIImageView]!
     var boatUpdatedBounds: CGRect!
     
-    var collided: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,11 +38,8 @@ class ViewController: UIViewController, subviewDelegate {
         scoreValue = 0
         scoreTimer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(ViewController.updateScore), userInfo: nil, repeats: true)
         
-        // Checks if boat has collided every second
-        collisionTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.boatCollides), userInfo: nil, repeats: true)
-        
         // Initializes the timer for the entire game (Finishes after 20 seconds)
-        gameTimer = Timer.scheduledTimer(timeInterval: 20, target: self, selector: #selector(ViewController.finishGame), userInfo: nil, repeats: true)
+        gameTimer = Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(ViewController.finishGame), userInfo: nil, repeats: true)
         
         
         // Setups the background for the game
@@ -70,36 +60,75 @@ class ViewController: UIViewController, subviewDelegate {
                       UIImage(named: "boat_8.png")!,
                       UIImage(named: "boat_9.png")!]
         boatImage.image = UIImage.animatedImage(with: imageArray, duration: 1)
-        boatUpdatedBounds = CGRect(origin: CGPoint(x: boatImage.center.x - (boatImage.bounds.width/2), y: boatImage.center.y - (boatImage.bounds.height/2)), size: boatImage.bounds.size)
+        boatUpdatedBounds = getUpdatedObjectBoundary(object: boatImage)
         
-        // Initialize array of obstacles
-        obstacleArray = [rock, rock2, rock3]
+        // Create obstacles and add to array
+        obstacleArray = []
+        createObstacles(num: 5)
         
-        // Enables obstacles to be given a behaviour
+        // Create coins and add to array
+        coinsArray = []
+        createCoins(num: 3)
+        
+        // Allows object bahaviours
         dynamicAnimator = UIDynamicAnimator(referenceView: self.view)
         
         // Enables the obstacles to be given a linear velocity
-        dynamicItemBehavior = UIDynamicItemBehavior(items: obstacleArray)
-        addObstacleMovements()
+        dynamicItemBehavior = UIDynamicItemBehavior(items: obstacleArray + coinsArray)
+        addObjectVelocity()
         dynamicAnimator.addBehavior(dynamicItemBehavior)
 
         // Enables the obstacles to be given a collision behaviour
-        collisionBehavior = UICollisionBehavior(items: obstacleArray)
-        collisionBehavior.translatesReferenceBoundsIntoBoundary = true
+        collisionBehavior = UICollisionBehavior(items: obstacleArray + coinsArray)
+        collisionBehavior.collisionDelegate = self
         dynamicAnimator.addBehavior(collisionBehavior)
+    }
+    
+    func createCoins(num: Int) {
+        let screenSize = UIScreen.main.bounds
+        var count = num
+        while count > 0 {
+            let randomXPos = CGFloat(arc4random_uniform(UInt32(screenSize.width)))
+            let randomYPos = CGFloat(arc4random_uniform(UInt32(screenSize.height)))
+            let coin = UIImageView(image: nil)
+            coin.image = UIImage(named: "coin.png")
+            coin.frame = CGRect(x:randomXPos, y: randomYPos, width: 20, height: 20)
+            coinsArray.append(coin)
+            self.view.addSubview(coin)
+            count = count-1
+        }
+    }
+    
+    func createObstacles(num: Int) {
+        let screenSize = UIScreen.main.bounds
+        var count = num
+        while count > 0 {
+            let randomXPos = CGFloat(arc4random_uniform(UInt32(screenSize.width)))
+            let randomYPos = CGFloat(arc4random_uniform(UInt32(screenSize.height)))
+            let obstacle = UIImageView(image: nil)
+            obstacle.image = UIImage(named: "rock.png")
+            obstacle.frame = CGRect(x:randomXPos, y: randomYPos, width: 30, height: 30)
+            obstacleArray.append(obstacle)
+            self.view.addSubview(obstacle)
+            count = count-1
+        }
     }
     
     // Executes this method everytime the boat is dragged to update its boundaries
     func boatDragged() {
         collisionBehavior.removeAllBoundaries()
-        boatUpdatedBounds = CGRect(origin: CGPoint(x: boatImage.center.x - (boatImage.bounds.width/2), y: boatImage.center.y - (boatImage.bounds.height/2)), size: boatImage.bounds.size)
+        boatUpdatedBounds = getUpdatedObjectBoundary(object: boatImage)
         collisionBehavior.addBoundary(withIdentifier: "boatRockCollisionPoint" as NSString, for: UIBezierPath(rect: boatUpdatedBounds))
     }
     
     // Gives linear velocity to an array of obstacles
-    func addObstacleMovements() {
+    func addObjectVelocity() {
         for ob in obstacleArray {
-            self.dynamicItemBehavior.addLinearVelocity(CGPoint(x: -50, y: 0), for: ob)
+            self.dynamicItemBehavior.addLinearVelocity(CGPoint(x: -75, y: 0), for: ob)
+        }
+        
+        for coin in coinsArray {
+            self.dynamicItemBehavior.addLinearVelocity(CGPoint(x: -75, y: 0), for: coin)
         }
         
     }
@@ -115,29 +144,37 @@ class ViewController: UIViewController, subviewDelegate {
     // Stops the game after 20 seconds and finalises the score.
     func finishGame() {
         scoreTimer.invalidate()
-        collisionTimer.invalidate()
         score.text = "Final Score: " + score.text!
         gameTimer.invalidate()
     }
     
-    // Check if the boat has collided with any of the obstacles every second
-    func boatCollides() {
-        for ob in obstacleArray {
-            if boatUpdatedBounds.intersects(getObstacleBounds(obstacle: ob)) {
-                scoreValue = scoreValue - 9402
-                ob.removeFromSuperview()
-                obstacleArray.remove(at: obstacleArray.index(of: ob)!)
+    // Check if the boat has collided with any of the obstacles
+    func collisionBehavior(_ behavior: UICollisionBehavior, beganContactFor item: UIDynamicItem, withBoundaryIdentifier identifier: NSCopying?, at p: CGPoint) {
+        let deductScore = 940
+        let increaseScore = 1200
+        if obstacleArray.contains(item as! UIImageView) {
+            if scoreValue > deductScore {
+                scoreValue = scoreValue - deductScore
             }
+            // Change color of avatar
+            boatImage.tintColor = UIColor.red
         }
+        
+        if coinsArray.contains(item as! UIImageView) {
+            scoreValue = scoreValue + increaseScore
+            (item as! UIImageView).image = nil
+            coinsArray.remove(at: coinsArray.index(of: item as! UIImageView)!)
+        }
+        
     }
-    
+
     // Returns the obstacle boundary that corresponds to their current position
-    func getObstacleBounds(obstacle: UIImageView) -> CGRect{
+    func getUpdatedObjectBoundary(object: UIImageView) -> CGRect{
         let obstacleBoundary: CGRect!
         obstacleBoundary = CGRect(
-            origin: CGPoint(x: obstacle.center.x - (obstacle.bounds.size.width/2),
-                            y: obstacle.center.y - (obstacle.bounds.size.height/2)),
-            size: obstacle.bounds.size)
+            origin: CGPoint(x: object.center.x - (object.bounds.size.width/2),
+                            y: object.center.y - (object.bounds.size.height/2)),
+            size: object.bounds.size)
         return obstacleBoundary
     }
     
